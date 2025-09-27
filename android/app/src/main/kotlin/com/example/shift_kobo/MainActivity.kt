fd6c7f4f4c1e514b4310fcd2c2ee4f12c05e1d4c
shi takahashi <shi-takahashi-999@gmail.com>
2025-09-27 20:27:55 +0900

    パッケージ名変更

--- android/app/src/main/kotlin/com/example/shift_kobo/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.shift_kobo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/io/github/shitakahashi/shiftkobo/MainActivity.kt
@@ -0,0 +1,5 @@
+package io.github.shitakahashi.shiftkobo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
